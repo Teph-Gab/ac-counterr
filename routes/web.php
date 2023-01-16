321188ffcd3f3b65a70e8282275c96c5f724997a
Teph Gab <tephgab@gmail.com>
2023-01-16 00:28:46 -0500

    Remove group route

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/admin/user', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin/user/{slug}', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/admin/user/{slug}', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::any('{slug}', [App\Http\Controllers\HomeController::class, 'index']);
